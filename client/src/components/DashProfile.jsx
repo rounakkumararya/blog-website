8792ab04c8519995b3f2e5534ee0ab45a5077a20
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-05-10 21:16:08 +0530

    complete profile img upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,28 +1,118 @@
 import { useSelector } from "react-redux";
-import { TextInput, Button, Label, FileInput } from "flowbite-react";
+import { TextInput, Button, Label, FileInput, Alert } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
+import { MdEdit } from "react-icons/md";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadingProgress, setImageFileUploadingProgress] =
+    useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+  const handleImgUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadingProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload image (File must be less than 2MB)"
+        );
+        setImageFileUploadingProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setImageFile(downloadUrl);
+        });
+      }
+    );
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl ">Profile</h1>
       <form className="flex flex-col gap-5 ">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="/image/*"
+          onChange={handleImgUpload}
+          ref={filePickerRef}
+          hidden
+        />
+        <div className="w-32 h-32 relative flex self-center  ">
+          {imageFileUploadingProgress && (
+            <CircularProgressbar
+              value={imageFileUploadingProgress || 0}
+              text={`${imageFileUploadingProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgb(62,152, 199, ${
+                    imageFileUploadingProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePic}
+            src={imageFileUrl || currentUser.profilePic}
             alt="profile picture"
-            className="rounded-full w-full h-full object-cover border-4 border-[lightgray]"
+            className={`shadow-md rounded-full w-full h-full object-cover border-4 border-[lightgray] ${
+              imageFileUploadingProgress &&
+              imageFileUploadingProgress < 100 &&
+              "opacity-60"
+            } `}
+          />
+
+          <MdEdit
+            className="absolute bottom-0 cursor-pointer right-0 w-6 h-6"
+            onClick={() => filePickerRef.current.click()}
           />
         </div>
-        <div>
-          <div>
-            <Label value="Upload Profile picture" />
-          </div>
-          <FileInput
-            id="file-upload-helper-text"
-            helperText="SVG, PNG, JPG or GIF (MAX. 800x400px)."
-          />
-        </div>
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
         <div className="flex flex-col gap-1">
           <Label value="Change username" />
           <TextInput
