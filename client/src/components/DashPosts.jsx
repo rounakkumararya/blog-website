4addcc1534a23a5105ab1712ac5adb2fd3a37902
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-06-15 19:51:43 +0530

    add update post feature

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -10,7 +10,7 @@ export default function DashPosts() {
   const [showMore, setShowMore] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const [postIdToDelete, setPostIdToDelete] = useState("");
-  console.log(userPosts);
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
