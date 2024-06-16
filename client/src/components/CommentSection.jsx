e0deefc5484803a412fec07dbd21aa2e0eaa2196
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-06-17 01:25:43 +0530

    show post comments in post page

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,24 +1,26 @@
-import { Alert, Button, Textarea } from "flowbite-react";
-import { useState } from "react";
+import { Alert, Button, TextInput, Textarea } from "flowbite-react";
+import { set } from "mongoose";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Comment from "./Comment";
 
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
-
+  const [comments, setComments] = useState([]);
+  console.log(comments);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
       return;
     }
-
     try {
       const res = await fetch("/api/comment/create", {
         method: "POST",
         headers: {
-          "Content-type": "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           content: comment,
@@ -30,32 +32,48 @@ export default function CommentSection({ postId }) {
       if (res.ok) {
         setComment("");
         setCommentError(null);
+        setComments([data, ...comments]);
       }
     } catch (error) {
       setCommentError(error.message);
     }
   };
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+          setComments(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getComments();
+  }, [postId]);
   return (
-    <div className=" max-w-2xl mx-auto w-full p-3">
+    <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
-        <div className="flex items-center gap-1 my-5 text-sm text-gray-500 ">
+        <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
           <p>Signed in as:</p>
           <img
             className="h-5 w-5 object-cover rounded-full"
-            src={currentUser.profilePic}
+            src={currentUser.profilePicture}
             alt=""
           />
           <Link
             to={"/dashboard?tab=profile"}
-            className="text-xs text-cyan-500 "
+            className="text-xs text-cyan-600 hover:underline"
           >
             @{currentUser.username}
           </Link>
         </div>
       ) : (
         <div className="text-sm text-teal-500 my-5 flex gap-1">
-          You must be signed in to Comment
-          <Link className="text-blue-400 hover:underline" to="/sign-in">
+          You must be signed in to comment.
+          <Link className="text-blue-500 hover:underline" to={"/sign-in"}>
             Sign In
           </Link>
         </div>
@@ -63,20 +81,20 @@ export default function CommentSection({ postId }) {
       {currentUser && (
         <form
           onSubmit={handleSubmit}
-          className="border border-teal-400 rounded-md p-3"
+          className="border border-teal-500 rounded-md p-3"
         >
           <Textarea
-            placeholder="Add your comment..."
+            placeholder="Add a comment..."
             rows="3"
             maxLength="200"
             onChange={(e) => setComment(e.target.value)}
             value={comment}
           />
-          <div className="flex justify-between items-center mt-5 ">
-            <p className="text-gray-500 text-xs ">
-              {200 - comment.length} characters left
+          <div className="flex justify-between items-center mt-5">
+            <p className="text-gray-500 text-xs">
+              {200 - comment.length} characters remaining
             </p>
-            <Button outline gradientDuoTone="pinkToOrange" type="submit">
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
               Submit
             </Button>
           </div>
@@ -86,6 +104,21 @@ export default function CommentSection({ postId }) {
             </Alert>
           )}
         </form>
+      )}
+      {comments.length === 0 ? (
+        <p className="text-sm my-5">No comments yet!</p>
+      ) : (
+        <>
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </>
       )}
     </div>
   );
