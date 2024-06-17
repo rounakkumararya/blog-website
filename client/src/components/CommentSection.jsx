fb2d076aa2a62e8c9af632cf8ff7297324c498b6
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-06-17 22:40:43 +0530

    complete edit feature on the client side

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -81,6 +81,13 @@ export default function CommentSection({ postId }) {
       console.log(error.message);
     }
   };
+  const handleEdit = async (comment, editedComment) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedComment } : c
+      )
+    );
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -144,7 +151,12 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment
+              key={comment._id}
+              comment={comment}
+              onLike={handleLike}
+              onEdit={handleEdit}
+            />
           ))}
         </>
       )}
