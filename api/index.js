130779731ec8d52fc03b921e0ff08f310ae1f377
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-08-06 13:15:45 +0530

    ready for deployment

--- api/index.js
+++ api/index.js
@@ -37,6 +37,10 @@ app.use("/api/comment", commentRoutes);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
